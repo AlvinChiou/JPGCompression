d2267bc05d3d4257e8601a4bf3daf1c402eb7645
AlvinChiou <alvinhsian@gmail.com>
2014-03-30 02:37:06 +0800

    Initial commit

---
+++ src/com/JPGCompression/Main.java
@@ -0,0 +1,10 @@
+package com.JPGCompression;
+
+public class Main {
+
+    public static void main(String[] args) {
+        jpgCompression jc = new jpgCompression();
+        jc.reduceImg("H:\\1.jpg", 600, 600, true);
+        System.out.print("À£ÁY§¹²¦!");
+    }
+}
