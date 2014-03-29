95e398d7dc018d2421c454bb310cec187052b11e
AlvinChiou <alvinhsian@gmail.com>
2014-03-30 02:41:11 +0800

    Rename to JPGCompression

--- src/com/JPGCompression/Main.java
+++ src/com/JPGCompression/Main.java
@@ -3,7 +3,7 @@ package com.JPGCompression;
 public class Main {
 
     public static void main(String[] args) {
-        jpgCompression jc = new jpgCompression();
+        JPGCompression jc = new JPGCompression();
         jc.reduceImg("H:\\1.jpg", 600, 600, true);
         System.out.print("À£ÁY§¹²¦!");
     }
